a0ee2b7105e7871b1cd9fe2f6c980a9acf5b48e3
jacostaf10 <jorge.acosta.freire@gmail.com>
2024-09-14 13:04:18 +0200

    Generic get

--- Sources/SwiftNet/SwiftNet.swift
+++ Sources/SwiftNet/SwiftNet.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public func get<T: Codable>(_ url: URL, of type: T.Type, parameters: [URLQueryItem] = [], decoder: JSONDecoder = .iso8601()) async throws -> T {
+    let (data, response) = try await URLSession.shared.data(for: url, parameters: parameters)
+    guard let httpResponse = response as? HTTPURLResponse else { throw NetworkError.invalidResponse }
+    guard httpResponse.statusCode == 200 else { throw NetworkError.status(httpResponse.statusCode) }
+    
+    do {
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        throw NetworkError.invalidData
+    }
+}
